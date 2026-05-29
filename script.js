f60f9e45dcac001bcb4bb3e52f2dff4c96af12a4
EZFelin <EFOliveira4@ucs.br>
2026-05-29 17:09:38 -0300

    Adicionei a função de ter vírgula no calculo!

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const btn = document.querySelectorAll('.btn');
 const display = document.querySelector('.display');
+
 let firstnumber = 0;
 let firstoperator = '';
 let calculated = false;
+
 function calculate(button) {
+    // IGUAL
     if (button.textContent == '=') {
         calculated = true;
         let result = '';
@@ -24,33 +27,56 @@ function calculate(button) {
             display.textContent = result;
         }
     }
+    // ADIÇÃO
     if (button.textContent == '+') {
         firstoperator = "+";
         firstnumber = display.textContent;
         display.textContent = '';
     }
+    // SUBTRAÇÃO
     if (button.textContent == '-') {
         firstoperator = "-";
         firstnumber = display.textContent;
         display.textContent = '';
     }
+    // MULTIPLICAÇÃO
     if (button.textContent == '*') {
         firstoperator = "*";
         firstnumber = display.textContent;
         display.textContent = '';
     }
+    // DIVISÃO
     if (button.textContent == '/') {
         firstoperator = "/";
         firstnumber = display.textContent;
         display.textContent = '';
     }
+    // LIMPAR O DISPLAY
     if (button.textContent == 'C') {
         display.textContent = '';
     }
-    if (button.textContent == '1' || button.textContent == '2' || button.textContent == '3' || button.textContent == '4' || button.textContent == '5' || button.textContent == '6' || button.textContent == '7' || button.textContent == '8' || button.textContent == '9' || button.textContent == '0') {
+    // NÚMEROS E PONTO
+    if (button.textContent == '1' ||
+        button.textContent == '2' ||
+        button.textContent == '3' ||
+        button.textContent == '4' ||
+        button.textContent == '5' ||
+        button.textContent == '6' ||
+        button.textContent == '7' ||
+        button.textContent == '8' || 
+        button.textContent == '9' ||
+        button.textContent == '0' ||
+        button.textContent == '.') {
         if (calculated) {
             display.textContent = '';
             calculated = false;
+        }
+        // EVITA QUE TENHA MAIS DE UM PONTO
+        if (
+            button.textContent == '.' &&
+            display.textContent.includes('.')
+        ) {
+            return;
         }
         display.textContent += button.textContent;
     }
